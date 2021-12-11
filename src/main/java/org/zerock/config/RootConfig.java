8576dbbf1a5fc6dfc55bfad60c3f7e5d491961a3
mg911004 <mg911003@naver.com>
2021-12-11 14:48:31 +0900

    dddd

--- src/main/java/org/zerock/config/RootConfig.java
+++ src/main/java/org/zerock/config/RootConfig.java
@@ -23,8 +23,8 @@ public class RootConfig {
 	    hikariConfig.setDriverClassName("net.sf.log4jdbc.sql.jdbcapi.DriverSpy");
 	    hikariConfig.setJdbcUrl("jdbc:log4jdbc:oracle:thin:@localhost:1521:XE");
 		
-		hikariConfig.setUsername("scott");
-		hikariConfig.setPassword("tiger");
+		hikariConfig.setUsername("javaqua");
+		hikariConfig.setPassword("1234");
 		
 		HikariDataSource dataSource = new HikariDataSource(hikariConfig);
 		
