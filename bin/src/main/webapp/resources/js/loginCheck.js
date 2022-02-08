fae3276e787a8881533eb4335da90f1043505900
tlqhrm <tlqhrm@naver.com>
2022-02-08 15:31:12 +0900

    Apply .gitignore

--- bin/src/main/webapp/resources/js/loginCheck.js
+++
@@ -1,8 +0,0 @@
-function loginCheck(id){
-	if( !id){
-		alert("로그인 후 이용 가능합니다.");
-		return false;	
-	}else{
-        return true;
-    }
-}
--- src/main/java/org/zerock/etc/ImagePath.java
+++
@@ -1,7 +0,0 @@
-package org.zerock.etc;
-
-public class ImagePath {
-	public static String get() {
-		return "tomcat/webapps/ROOT/resources/upload/";
-	}
-}
